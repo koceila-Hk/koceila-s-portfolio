dded4245944b012caa63940462e6d5e22c06a0c4
kouss <koceila.habarek94@gmail.com>
2024-02-22 01:15:26 +0100

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,8 +27,51 @@ window.onscroll = () => {
     console.log(navBar);
 };
 
+// ========== HANDLE INTERSECTION =========
+
+let ratio = 0.1;
+let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: ratio
+};
+
+let handleIntersect = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible');
+            observer.unobserve(entry.target);
+        }        
+    });
+};
+
+let observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('[class*="reveal-"]').forEach(function(r) {
+    observer.observe(r)
+});
+
+
+
 // ========== FOOTER ==========
 
 const year = document.querySelector('.year');
 const date = new Date();
 year.innerHTML += date.getFullYear();
+
+
+
+
+
+// Sélectionnez tous les éléments avec la classe .portfolioCard-content
+const portfolioCards = document.querySelectorAll('.portfolioCard-content');
+
+// Ajoutez un gestionnaire d'événements pour chaque élément
+portfolioCards.forEach(card => {
+    card.addEventListener('mouseover', () => {
+        card.classList.add('flip'); // Ajoute la classe flip lors du survol
+    });
+
+    card.addEventListener('mouseout', () => {
+        card.classList.remove('flip'); // Retire la classe flip lorsque la souris quitte l'élément
+    });
+});
